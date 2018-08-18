4faeb73a81effb171caf638bc4a78e2dcca241bc
Volodymyr Petrusha <volodymyr.petrusha@osi.ca.gov>
2018-08-17 17:06:28 -0700

    AARA-545 Contacts table implementation

---
+++ app/javascript/contacts/contacts_list/ContacsTable.jsx
@@ -0,0 +1,80 @@
+import React, { Fragment } from 'react'
+import ReactTable from 'react-table'
+import Moment from 'moment'
+import {urlPrefixHelper} from 'helpers/url_prefix_helper.js.erb'
+import {sortbyDate} from 'search/common/commonUtils'
+
+export default class ContactsTable extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      contacts: props.contacts
+    }
+  }
+
+  render () {
+    const {contacts} = this.state
+    return (
+      <Fragment>
+        <ReactTable
+          className={'contacts-table'}
+          data={contacts}
+          columns={columns}
+          defaultPageSize={contacts.length}
+          showPagination={false}
+          sortable={true}
+          resizable={false}
+          noDataText=''
+          defaultSorted={[
+            {
+              id: 'date',
+              desc: true
+            }
+          ]}
+        />
+      </Fragment>
+    )
+  }
+}
+
+const columns = [
+  {
+    Header: h => <span> Date <i className='fa fa-unsorted' /></span>,
+    headerClassName: 'contacts-th',
+    id: 'date',
+    accessor: d => {
+      return Moment(d.date)
+        .local()
+        .format('MM/DD/YYYY')
+    },
+    maxWidth: 100,
+    className: 'contacts-td',
+    sortMethod: (a, b) => sortbyDate(a, b)
+  },
+  {
+    Header: h => <span> Method of Contacts <i className='fa fa-unsorted' /></span>,
+    headerClassName: 'contacts-th',
+    id: 'contact_method',
+    accessor: 'contact_method.value',
+    maxWidth: 160,
+    className: 'contacts-td'
+  },
+  {
+    Header: h => <span> Type <i className='fa fa-unsorted' /></span>,
+    headerClassName: 'contacts-th',
+    id: 'in_person_contact_data',
+    accessor: 'in_person_contact_data.visit_type.value',
+    maxWidth: 150,
+    className: 'contacts-td'
+  },
+  {
+    Header: 'Title',
+    headerClassName: 'contacts-th',
+    id: 'contact',
+    sortable: false,
+    accessor: d => d,
+    maxWidth: 600,
+    className: 'contacts-td',
+    Cell: row => (<a href={urlPrefixHelper('/contacts/' + row.value.id + '/edit')}>{row.value.title}</a>)
+  }
+]
